3d249c0f61155d7694155f166bb66633cd063f4c
Yisroel Forta <yforta@google.com>
2024-02-01 19:54:41 +0000

    Developer Profiling APIs
    Add APIs only as no-op
    
    Test: m, presubmit
    API-Coverage-Bug: 293957254
    Bug: 293957254
    Change-Id: Ie627e672f46fbae89997f90c1ad9fc3de04f8d5f

--- framework/java/android/os/ProfilingManager.java
+++ framework/java/android/os/ProfilingManager.java
@@ -15,8 +15,76 @@
  */
 package android.os;
 
-/** @hide */
+import android.annotation.FlaggedApi;
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.content.Context;
+import android.os.OutcomeReceiver;
+import android.os.profiling.Flags;
+
+import java.lang.Exception;
+import java.util.concurrent.Executor;
+import java.util.function.Consumer;
+
+/**
+ * API for apps to request and listen for app specific profiling.
+ */
+@FlaggedApi(Flags.FLAG_TELEMETRY_APIS)
 public class ProfilingManager {
-    /** @hide **/
-    public void placeholder() {}
+
+    /** @hide */
+    public ProfilingManager(Context context) {}
+
+    /**
+     * Request profiling via perfetto.
+     *
+     * <p class="note"> Note: use of this API directly is not recommended for most use cases.
+     * Please use the higher level wrappers provided by androidx that will construct the request
+     * correctly based on available options and simplified user provided request parameters.</p>
+     *
+     * <p class="note"> Note: requests are not guaranteed to be filled.</p>
+     *
+     * <p class="note"> Note: a listener must be set for the request to be considered for
+     * fulfillment. Listeners can be set in this method, with {@see #registerForProfilingResult},
+     * or both. If no listener is set the request will be discarded.</p>
+     *
+     * @param profilingRequest byte array representation of ProfilingRequest proto containing all
+     *                         necessary information about the collection being requested.
+     * @param tag Caller defined data to help identify the output.
+     * @param cancellationSignal for caller requested cancellation.
+     * @param executor The executor to call back with.
+     * @param listener Listener to be triggered with result.
+     */
+    public void requestProfiling(
+            @NonNull byte[] profilingRequest,
+            @Nullable String tag,
+            @Nullable CancellationSignal cancellationSignal,
+            @Nullable Executor executor,
+            @Nullable Consumer<ProfilingResult> listener) {
+        // TODO b/293957254
+    }
+
+    /**
+     * Register a listener to be called for all profiling results.
+     *
+     * @param executor The executor to call back with.
+     * @param listener Listener to be triggered with result.
+     */
+    public void registerForProfilingResults(
+            @NonNull Executor executor,
+            @NonNull Consumer<ProfilingResult> listener) {
+        // TODO b/293957254
+    }
+
+    /**
+     * Unregister a listener to be called for all profiling results. If no listener is provided,
+     * all listeners for this process that were not submitted with a currently running trace will
+     * be removed.
+     *
+     * @param listener Listener to unregister and no longer be triggered with the result.
+     */
+    public void unregisterForProfilingResults(
+            @Nullable Consumer<ProfilingResult> listener) {
+        // TODO b/293957254
+    }
 }
