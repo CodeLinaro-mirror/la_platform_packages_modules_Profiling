ef352e1609dd4760df2f30b0252f6c1ed43b022d
Steven Terrell <steventerrell@google.com>
2024-03-21 18:22:09 +0000

    Setup for calling trace_redactor
    Add the initial setup and flow for redacting traces before delivering to
    app storage.  With this change when a trace is completed a new processes
    will be created to run the trace_redactor binary.  The newly redacted
    file will then be delivered to app storage and the unredacted file will
    be deleted.
    
    Bug: 327423523
    Test: Atest CtsProfilingModuleTests
    Change-Id: Ic8f11584b698650657329e0e2d8f32db63a3e14b

--- service/java/com/android/os/profiling/ProfilingService.java
+++ service/java/com/android/os/profiling/ProfilingService.java
@@ -44,6 +44,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Locale;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -61,10 +63,15 @@ public class ProfilingService extends IProfilingService.Stub {
     private static final String OUTPUT_FILE_HEAP_PROFILE_SUFFIX = ".perfetto-heap-profile";
     private static final String OUTPUT_FILE_STACK_SAMPLING_SUFFIX = ".perfetto-stack-sample";
     private static final String OUTPUT_FILE_TRACE_SUFFIX = ".perfetto-trace";
+    private static final String OUTPUT_FILE_UNREDACTED_TRACE_SUFFIX = ".perfetto-trace-unredacted";
 
     private static final int TAG_MAX_CHARS_FOR_FILENAME = 20;
 
     private static final int PERFETTO_DESTROY_DEFAULT_TIMEOUT_MS = 10 * 1000;
+
+    private static final int REDACTION_MAX_RUNTIME_ALLOTTED_MS = 20  * 1000;
+
+    private static final int REDACTION_CHECK_FREQUENCY_MS = 2 * 1000;
 
     private final int PERFETTO_DESTROY_TIMEOUT_MS;
 
@@ -224,10 +231,18 @@ public class ProfilingService extends IProfilingService.Stub {
 
         }
 
-        // Now start the trace.
-        session.setFileName(OUTPUT_FILE_PREFIX
+        String baseFileName = OUTPUT_FILE_PREFIX
                 + (tag.isEmpty() ? "" : OUTPUT_FILE_SECTION_SEPARATOR + tag)
-                + OUTPUT_FILE_SECTION_SEPARATOR + getFormattedDate() + suffix);
+                + OUTPUT_FILE_SECTION_SEPARATOR + getFormattedDate();
+
+        // Only trace files will go through the redaction process, set the name here for the file
+        // that will be created later when results are processed.
+        if (session.getProfilingType() == ProfilingManager.PROFILING_TYPE_SYSTEM_TRACE) {
+            session.setRedactedFileName(baseFileName + OUTPUT_FILE_TRACE_SUFFIX);
+        }
+
+        session.setFileName(baseFileName + suffix);
+
         try {
             ProcessBuilder pb = new ProcessBuilder("/system/bin/perfetto", "-o",
                     TEMP_TRACE_PATH + session.getFileName(), "-c", "-", "--txt");
@@ -326,25 +341,26 @@ public class ProfilingService extends IProfilingService.Stub {
      * and return a {@link ParcelFileDescriptor} to copy the temporary file contents to.
      * Finally, delete the temporary file.
      *
-     * @return true if file was successfully copied and cleaned up, false if not.
      */
-    private boolean moveFileToAppStorage(TracingSession session) {
+    private void moveFileToAppStorage(TracingSession session) {
         if (!mResultCallbacks.contains(session.getUid())) {
             // No callback, nowhere to notify with result.
             if (DEBUG) Log.d(TAG, "No callback to ProfilingManager, callback dropped.");
-            // TODO: queue this and try next time the uid registers a receiver.
-            // TODO: run a cleanup of old results based on a max size and time.
-            return false;
+            // TODO: b/333456430 queue this and try next time the uid registers a receiver.
+            // TODO: b/333456916 run a cleanup of old results based on a max size and time.
+            return;
         }
 
         // Setup file streams.
-        File tempPerfettoFile = new File(TEMP_TRACE_PATH + session.getFileName());
+        File tempResultFile = new File(TEMP_TRACE_PATH
+                + (session.getProfilingType() == ProfilingManager.PROFILING_TYPE_SYSTEM_TRACE
+                ? session.getRedactedFileName() : session.getFileName()));
         FileInputStream tempPerfettoFileInStream = null;
         FileOutputStream appFileOutStream = null;
         ParcelFileDescriptor pfd = null;
         boolean failed = false;
         try {
-            tempPerfettoFileInStream = new FileInputStream(tempPerfettoFile);
+            tempPerfettoFileInStream = new FileInputStream(tempResultFile);
         } catch (IOException e) {
             // IO Exception opening temp perfetto file. No result.
             if (DEBUG) Log.d(TAG, "Exception opening temp perfetto file.", e);
@@ -354,12 +370,12 @@ public class ProfilingService extends IProfilingService.Stub {
             try {
                 pfd = mResultCallbacks.get(session.getUid())
                         .generateFile(session.getAppFilePath() + OUTPUT_FILE_RELATIVE_PATH,
-                            session.getFileName());
+                            tempResultFile.getName());
                 appFileOutStream = new FileOutputStream(pfd.getFileDescriptor());
             } catch (RemoteException e) {
                 // Binder exception getting file.
                 if (DEBUG) Log.d(TAG, "Binder exception getting file.", e);
-                // TODO: queue this and try next time the uid registers a receiver.
+                // TODO: b/333456430 queue this and try next time the uid registers a receiver.
                 failed = true;
             }
         }
@@ -371,7 +387,7 @@ public class ProfilingService extends IProfilingService.Stub {
             } catch (IOException e) {
                 // Exception writing to local app file.
                 if (DEBUG)  Log.d(TAG, "Exception writing to local app file.", e);
-                // TODO: queue this and try again later.
+                // TODO: b/333456430 queue this and try again later.
                 failed = true;
             }
         }
@@ -379,7 +395,7 @@ public class ProfilingService extends IProfilingService.Stub {
         // Finally delete the temp file.
         if (!failed) {
             try {
-                tempPerfettoFile.delete();
+                tempResultFile.delete();
             } catch (SecurityException e) {
                 // Exception deleting temp file.
                 if (DEBUG) Log.d(TAG, "Permissions exception deleting temp file.", e);
@@ -409,13 +425,7 @@ public class ProfilingService extends IProfilingService.Stub {
             }
         }
 
-        return !failed;
-    }
-
-    private void processResult(TracingSession session) {
-        // todo: start redaction in its own process.
-        boolean success = moveFileToAppStorage(session);
-        if (success) {
+        if (!failed) {
             processResultCallback(session, ProfilingResult.ERROR_NONE, null);
             mTracingSessions.remove(session.getKey());
         } else {
@@ -424,6 +434,109 @@ public class ProfilingService extends IProfilingService.Stub {
             if (DEBUG) Log.d(TAG, "Couldn't move file to app storage.");
             processResultCallback(session, ProfilingResult.ERROR_FAILED_POST_PROCESSING, null);
         }
+    }
+
+
+    // processResult will be called after every profiling type is collected, traces will go
+    // through a redaction process before being returned to the client.  All other profiling types
+    // can be returned as is.
+    private void processResult(TracingSession session) {
+        if (session.getProfilingType() == ProfilingManager.PROFILING_TYPE_SYSTEM_TRACE) {
+            handleTraceResult(session);
+        } else {
+            moveFileToAppStorage(session);
+        }
+    }
+
+    //TODO b/331684767 need to figure out what errors from redaction can be retried.
+    private void handleTraceResult(TracingSession session) {
+        try {
+            // We need to create an empty file for the redaction process to write the output into.
+            File emptyRedactedTraceFile = new File(TEMP_TRACE_PATH
+                    + session.getRedactedFileName());
+            emptyRedactedTraceFile.createNewFile();
+        } catch (Exception exception) {
+            if (DEBUG) Log.e(TAG, "Creating empty redacted file failed.", exception);
+            processResultCallback(session, ProfilingResult.ERROR_FAILED_POST_PROCESSING, null);
+            return;
+        }
+
+        try {
+            // Start the redaction process and log the time of start.  Redaction has
+            // REDACTION_MAX_RUNTIME_ALLOTTED_MS to complete. Redaction status will be checked every
+            // REDACTION_CHECK_FREQUENCY_MS.
+            ProcessBuilder redactionProcess = new ProcessBuilder("/system/bin/trace_redactor",
+                    TEMP_TRACE_PATH + session.getFileName(),
+                    TEMP_TRACE_PATH + session.getRedactedFileName(),
+                    session.getPackageName());
+            session.setActiveRedaction(redactionProcess.start());
+            session.setRedactionStartTimeMs(System.currentTimeMillis());
+        } catch (Exception exception) {
+            if (DEBUG) Log.e(TAG, "Redaction failed to run completely.", exception);
+            processResultCallback(session, ProfilingResult.ERROR_FAILED_POST_PROCESSING, null);
+            return;
+        }
+        session.setProcessResultRunnable(new Runnable() {
+
+            @Override
+            public void run() {
+                checkRedactionStatus(session);
+            }
+        });
+        // TODO b/333476809 adjust frequency time once we have a better
+        //  understanding of redaction performance.
+        getHandler().postDelayed(session.getProcessResultRunnable(),
+                REDACTION_CHECK_FREQUENCY_MS);
+    }
+
+    private void checkRedactionStatus(TracingSession session) {
+        // Check if redaction is complete.
+        if (!session.getActiveRedaction().isAlive()) {
+            handleRedactionComplete(session);
+            session.setProcessResultRunnable(null);
+            return;
+        }
+
+        // Check if we are over the REDACTION_MAX_RUNTIME_ALLOTTED_MS threshold.
+        if ((System.currentTimeMillis() - session.getRedactionStartTimeMs())
+                > REDACTION_MAX_RUNTIME_ALLOTTED_MS) {
+            if (DEBUG) Log.d(TAG, "Redaction process has timed out");
+
+            session.getActiveRedaction().destroyForcibly();
+            session.setProcessResultRunnable(null);
+            processResultCallback(session, ProfilingResult.ERROR_FAILED_POST_PROCESSING,
+                    null);
+
+            return;
+        }
+        getHandler().postDelayed(session.getProcessResultRunnable(),
+                Math.min(REDACTION_CHECK_FREQUENCY_MS, REDACTION_MAX_RUNTIME_ALLOTTED_MS
+                        - (System.currentTimeMillis() - session.getRedactionStartTimeMs())));
+
+    }
+
+    private void handleRedactionComplete(TracingSession session) {
+        int redactionErrorCode = session.getActiveRedaction().exitValue();
+        if (redactionErrorCode != 0) {
+            // Redaction process failed.
+            if (DEBUG) {
+                Log.d(TAG, String.format("Redaction processed failed with error code: %s",
+                        redactionErrorCode));
+            }
+            processResultCallback(session, ProfilingResult.ERROR_FAILED_POST_PROCESSING, null);
+            return;
+        }
+
+        // At this point redaction has completed successfully it is safe to delete the unredacted
+        // trace file.
+        // TODO b/331988161 Delete after file is delivered to app.
+        try {
+            Files.delete(Path.of(TEMP_TRACE_PATH + session.getFileName()));
+        } catch (Exception exception) {
+            if (DEBUG) Log.e(TAG, "Failed to delete unredacted file.", exception);
+        }
+
+        moveFileToAppStorage(session);
     }
 
     private Handler getHandler() {
@@ -460,7 +573,7 @@ public class ProfilingService extends IProfilingService.Stub {
             case ProfilingManager.PROFILING_TYPE_STACK_SAMPLING:
                 return OUTPUT_FILE_STACK_SAMPLING_SUFFIX;
             case ProfilingManager.PROFILING_TYPE_SYSTEM_TRACE:
-                return OUTPUT_FILE_TRACE_SUFFIX;
+                return OUTPUT_FILE_UNREDACTED_TRACE_SUFFIX;
             default:
                 throw new IllegalArgumentException("Invalid profiling type");
         }
