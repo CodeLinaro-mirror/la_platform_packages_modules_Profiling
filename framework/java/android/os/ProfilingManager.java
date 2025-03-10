bdeae8130b26fd6de772733d65f8985b9c591981
Yisroel Forta <yforta@google.com>
2025-03-10 21:46:31 +0000

    Update javadoc
    - Add links to androidx docs
    - Add rate limiting details to register triggers
    
    Test: n/a
    Bug: 399139855
    Flag: EXEMPT - docs only
    Change-Id: I68af960003a949f734cce9c22491ef05f5a3ae1a

--- framework/java/android/os/ProfilingManager.java
+++ framework/java/android/os/ProfilingManager.java
@@ -224,8 +224,10 @@ public final class ProfilingManager {
      *
      * <p class="note">
      *   Note: use of this API directly is not recommended for most use cases.
-     *   Consider using the higher level wrappers provided by AndroidX that will construct the
-     *   request correctly, supporting available options with simplified request parameters
+     *   Consider using the
+     *   <a href="https://developer.android.com/reference/androidx/core/os/Profiling">higher level
+     *   wrappers provided by AndroidX</a> that will construct the request correctly, supporting
+     *   available options with simplified request parameters.
      * </p>
      *
      * <p>
@@ -256,7 +258,9 @@ public final class ProfilingManager {
      *                  {@link android.os.ProfilingResult#ERROR_FAILED_INVALID_REQUEST}. If the
      *                  values for the parameters are out of supported range, the closest possible
      *                  in range value will be chosen.
-     *                  Use of androidx wrappers is recommended over generating this directly.
+     *                  Use of <a href=
+     *                  "https://developer.android.com/reference/androidx/core/os/Profiling">
+     *                  androidx wrappers</a> is recommended over generating this directly.
      * @param tag Caller defined data to help identify the output.
      *                  The first 20 alphanumeric characters, plus dashes, will be lowercased
      *                  and included in the output filename.
@@ -436,13 +440,18 @@ public final class ProfilingManager {
     }
 
     /**
+     * <p>
      * Register the provided list of triggers for this process.
+     * </p>
      *
-     * Profiling triggers are system triggered events that an app can register interest in receiving
-     * profiling of. There is no guarantee that these triggers will be filled. Results, if
-     * available, will be delivered only to a global listener added using
-     * {@link #registerForAllProfilingResults}.
+     * <p>
+     * Profiling triggers are system events that an app can register interest in, and then receive
+     * profiling data when any of the registered triggers occur. There is no guarantee that these
+     * triggers will be filled. Results, if available, will be delivered only to a global listener
+     * added using {@link #registerForAllProfilingResults}.
+     *</p>
      *
+     * <p>
      * Only one of each trigger type can be added at a time.
      * <ul>
      * <li>If the provided list contains a trigger type that is already registered then the new one
@@ -450,6 +459,13 @@ public final class ProfilingManager {
      * <li>If the provided list contains more than one trigger object for a trigger type then only
      *     one will be kept.</li>
      * </ul>
+     * </p>
+     *
+     * <p>
+     * Apps can define their own per-trigger rate limiting to help ensure they receive results
+     * aligned with their needs. More details can be found at
+     * {@link ProfilingTrigger.Builder#setRateLimitingPeriodHours}.
+     * </p>
      */
     @FlaggedApi(Flags.FLAG_SYSTEM_TRIGGERED_PROFILING_NEW)
     public void addProfilingTriggers(@NonNull List<ProfilingTrigger> triggers) {
