71795bab36ba1933265b933a9f8f56443f4f0598
Edgar Arriaga <edgararriaga@google.com>
2025-09-03 20:31:32 +0000

    Add more logging for local debugging in ProfilingManager
    Test: Manual
    Bug: 442888679
    Flag: NONE compiler dropped logging for local profiling
    
    Change-Id: Ifb4ff4c71b9734bad07fb9efbac2eba898f2d8ca

--- service/java/com/android/os/profiling/ProfilingService.java
+++ service/java/com/android/os/profiling/ProfilingService.java
@@ -750,11 +750,18 @@ public class ProfilingService extends IProfilingService.Stub {
      */
     @VisibleForTesting
     public void advanceTracingSession(TracingSession session, @Nullable TracingState newState) {
+        if (DEBUG) {
+            Log.d(TAG, "Advance Tracing State to "
+                    + (newState != null ? newState.getValue() : "null"));
+        }
         if (newState == null) {
             if (session.getRetryCount() == 0) {
                 // The new state should only be null if this is triggered from the queue in which
                 // case the retry count should be greater than 0. If retry count is 0 here then
                 // we're in an unexpected state. Cleanup and discard. Result will be lost.
+                if (DEBUG) {
+                    Log.d(TAG, "advanceTracingSession error: unexpected null state");
+                }
                 cleanupTracingSession(session);
                 return;
             }
@@ -764,12 +771,18 @@ public class ProfilingService extends IProfilingService.Stub {
             // loop. Terminate this attempt and increment the retry count to ensure there's a
             // path to breaking out of a potential infinite queue retries.
             session.incrementRetryCount();
+            if (DEBUG) {
+                Log.d(TAG, "advanceTracingSession error: trying to set same state as before");
+            }
             return;
         } else if (newState.getValue() < session.getState().getValue()) {
             // This should also never happen.
             // States should always move forward. If the state is trying to move backwards then
             // we don't actually know what to do next. Clean up the session and delete
             // everything. Results will be lost.
+            if (DEBUG) {
+                Log.d(TAG, "advanceTracingSession error: moving backwards");
+            }
             cleanupTracingSession(session);
             return;
         } else {
@@ -1555,6 +1568,9 @@ public class ProfilingService extends IProfilingService.Stub {
                     session.getParams(), LoggingHelper.REQUEST_RESULT_PROFILING_STARTED,
                     mRateLimiter.isRateLimiterDisabled());
         } else {
+            if (DEBUG) {
+                Log.d(TAG, "Failed to start profiling.");
+            }
             session.setError(ProfilingResult.ERROR_FAILED_EXECUTING, "Trace couldn't be started");
 
             LoggingHelper.logProfilingRequest(session.getUid(), session.getProfilingType(),
@@ -1663,6 +1679,9 @@ public class ProfilingService extends IProfilingService.Stub {
     @Nullable
     private Process startProfilingProcess(byte[] config, String outputFile) {
         try {
+            if (DEBUG) {
+                Log.d(TAG, "Starting perfetto process profile output file=" + outputFile);
+            }
             ProcessBuilder processBuilder = new ProcessBuilder("/system/bin/perfetto", "-o",
                     outputFile, "-c", "-");
             Process activeProfiling = processBuilder.start();
@@ -1988,6 +2007,11 @@ public class ProfilingService extends IProfilingService.Stub {
 
         if (session.getActiveTrace().isAlive()
                 && processingTimeRemaining >= 0) {
+            if (DEBUG) {
+                Log.d(TAG, "Profiling not yet finished. processingTimeRemaining="
+                        + processingTimeRemaining + " reschedule check in "
+                        + Math.min(mProfilingRecheckDelayMs, processingTimeRemaining));
+            }
             // still running and under max allotted processing time, reschedule the check.
             getHandler().postDelayed(session.getProcessResultRunnable(),
                     Math.min(mProfilingRecheckDelayMs, processingTimeRemaining));
@@ -2027,6 +2051,10 @@ public class ProfilingService extends IProfilingService.Stub {
     /** Stop active profiling for the given session key. */
     private void stopProfiling(String key, int loggingReason) {
         TracingSession session = mActiveTracingSessions.get(key);
+        if (DEBUG) {
+            Log.d(TAG, "stopProfiling for session="
+                        + session.getFileName() + " loggingReason=" + loggingReason);
+        }
         stopProfiling(session, loggingReason);
     }
 
@@ -2056,6 +2084,9 @@ public class ProfilingService extends IProfilingService.Stub {
                     TimeUnit.MILLISECONDS)) {
                 if (DEBUG) Log.d(TAG, "Stopping of running trace process timed out.");
                 return;
+            }
+            if (DEBUG) {
+                Log.d(TAG, "Stopped running trace process.");
             }
         } catch (InterruptedException e) {
             if (DEBUG) Log.d(TAG, "Stopping of running trace error occurred.", e);
@@ -2223,6 +2254,10 @@ public class ProfilingService extends IProfilingService.Stub {
     @VisibleForTesting
     public void handleRedactionRequiredResult(TracingSession session) {
         try {
+            if (DEBUG) {
+                Log.d(TAG, "Start redaction, create empty file for redactor output="
+                                + session.getRedactedFileName());
+            }
             // We need to create an empty file for the redaction process to write the output into.
             File emptyRedactedTraceFile = new File(TEMP_TRACE_PATH
                     + session.getRedactedFileName());
@@ -2451,6 +2486,9 @@ public class ProfilingService extends IProfilingService.Stub {
      */
     private void cleanupTracingSession(TracingSession session,
             @Nullable List<TracingSession> queuedSessions) {
+        if (DEBUG) {
+            Log.d(TAG, "cleanupTracingSession for fileName=" + session.getFileName());
+        }
         synchronized (mLock) {
             if (mKeepResultInTempDir) {
                 // If {@link mKeepResultInTempDir} is enabled, don't cleanup anything. Continue
@@ -2485,6 +2523,9 @@ public class ProfilingService extends IProfilingService.Stub {
             boolean deleteUnredacted) {
         if (deleteRedacted) {
             try {
+                if (DEBUG) {
+                    Log.d(TAG, "delete redacted file=" + session.getRedactedFileName());
+                }
                 Files.deleteIfExists(Path.of(TEMP_TRACE_PATH + session.getRedactedFileName()));
             } catch (Exception exception) {
                 if (DEBUG) Log.e(TAG, "Failed to delete file.", exception);
@@ -2493,6 +2534,9 @@ public class ProfilingService extends IProfilingService.Stub {
 
         if (deleteUnredacted) {
             try {
+                if (DEBUG) {
+                    Log.d(TAG, "delete unredacted file session=" + session.getFileName());
+                }
                 Files.deleteIfExists(Path.of(TEMP_TRACE_PATH + session.getFileName()));
             } catch (Exception exception) {
                 if (DEBUG) Log.e(TAG, "Failed to delete file.", exception);
@@ -2519,6 +2563,10 @@ public class ProfilingService extends IProfilingService.Stub {
         if (DEBUG && session.getProfilingStartTimeMs() == 0) {
             Log.e(TAG, "Attempting to move session to queue without a start time set.",
                     new Throwable());
+        }
+
+        if (DEBUG) {
+            Log.d(TAG, "Add to queue session with file=" + session.getFileName());
         }
 
         List<TracingSession> queuedResults = mQueuedTracingResults.get(session.getUid());
