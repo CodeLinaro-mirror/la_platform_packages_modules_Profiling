eaa507372d072a57e3f112c22a774e1e3f3be276
Hao Liu <haoliuu@google.com>
2025-12-09 13:53:39 -0800

    Revert "Add experiment for OOM trigger"
    This reverts commit c60998a959da8d1a3bc3a9dda1cd631f3ca31438.
    
    Reason for revert: Revert the oom experiment and flag.
    
    Bug: 448618175
    Change-Id: I2b1bd6e0c8b57ee71399704aad992e7aa87a8de9

--- service/java/com/android/os/profiling/ProfilingService.java
+++ service/java/com/android/os/profiling/ProfilingService.java
@@ -2602,17 +2602,6 @@ public class ProfilingService extends IProfilingService.Stub {
             int profilingType,
             @Nullable IProfilingTriggerCallback callback) {
         ProfilingTriggerData trigger = getTriggerDataObject(uid, packageName, triggerType);
-
-        if (trigger == null
-                && Flags.profilingTriggerOom()
-                && triggerType == ProfilingTrigger.TRIGGER_TYPE_OOM
-                && Flags.oomTriggerExperimentDoNotRelease()) {
-            // In order to evaluate perf impact of the oom trigger which delays app death, create a
-            // fake trigger object if the real one is non-existent, and proceed with that object.
-            // The object will not be saved in this flow so it will only apply to this session.
-            trigger = new ProfilingTriggerData(uid, packageName, triggerType, 0);
-        }
-
         if (trigger == null) {
             // No trigger object, process isn't registered for this trigger.
             performTriggerCallback(callback);
