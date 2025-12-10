b963989167400ecdfc00018592254cb2737a5244
Costin Manolache <costin@google.com>
2025-12-10 21:42:25 +0000

    Add a dumpsys command to check the rules.
    This is required for testing/troubleshooting.
    
    Flag: android.os.profiling.anomaly.flags.anomaly_detector_core
    Test: atest MtsAnomalyDetectorTests
    Change-Id: Ie285f987c5031b4387364ec276f761336bd5c1e3
    Bug: 468066157

--- anomaly-detector/service/java/com/android/os/profiling/anomaly/AnomalyDetectorService.java
+++ anomaly-detector/service/java/com/android/os/profiling/anomaly/AnomalyDetectorService.java
@@ -21,6 +21,7 @@ import static android.Manifest.permission.CONFIGURE_ANOMALY_DETECTOR;
 import android.annotation.FlaggedApi;
 import android.annotation.PermissionManuallyEnforced;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.os.OutcomeReceiver;
 import android.os.profiling.anomaly.IAnomalyDetectorService;
@@ -51,6 +52,8 @@ import com.android.server.LocalManagerRegistry;
 import com.android.server.SystemService;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Executor;
@@ -70,7 +73,7 @@ import java.util.concurrent.Executors;
 public final class AnomalyDetectorService extends SystemService {
     private static final String TAG = "AnomalyDetectorService";
 
-    private final BinderService mBinderService;
+    @VisibleForTesting final BinderService mBinderService;
 
     @VisibleForTesting final AnomalyDetectorManagerLocal mLocalManager;
 
@@ -162,17 +165,53 @@ public final class AnomalyDetectorService extends SystemService {
     }
 
     /** Implementation of the IAnomalyDetectorService binder service. */
-    private static final class BinderService extends IAnomalyDetectorService.Stub {
+    @VisibleForTesting
+    static final class BinderService extends IAnomalyDetectorService.Stub {
         private final Context mContext;
 
-        @SuppressWarnings("unused") // This will be used once APIs are implemented.
-        private final AnomalyDetectorController mController;
+        final AnomalyDetectorController mController;
 
         BinderService(Context context, AnomalyDetectorController controller) {
             mContext = context;
             mController = controller;
         }
 
+        /**
+         * dump implements the 'dumpsys anomaly_detector', currently for debugging (no guarantees on
+         * format stability for now)
+         *
+         * @param fd The raw file descriptor that the dump is being sent to.
+         * @param pw The file to which you should dump your state. This will be closed for you after
+         *     you return.
+         * @param args additional arguments to the dump request.
+         */
+        @PermissionManuallyEnforced
+        @Override
+        protected void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
+            if (mContext.checkCallingOrSelfPermission(android.Manifest.permission.DUMP)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            Set<RuleInternal> rules = mController.getRules();
+            if (rules != null) {
+                pw.println("Rules:");
+                for (RuleInternal r : rules) {
+                    pw.println(
+                            r.getConditionType()
+                                    + " "
+                                    + r.getAnomalyActions()
+                                    + " "
+                                    + r.getRuleCondition());
+                }
+            }
+        }
+
+        /**
+         * setRules is the entrypoint from allowed apps that interact with the control plane (server
+         * or local) to inject the rules.
+         *
+         * @param ruleParcelList The list of rules to set.
+         */
         @Override
         @PermissionManuallyEnforced
         public void setRules(List<RuleParcel> ruleParcelList) {
--- anomaly-detector/service/java/com/android/os/profiling/anomaly/internal/AnomalyDetectorControllerImpl.java
+++ anomaly-detector/service/java/com/android/os/profiling/anomaly/internal/AnomalyDetectorControllerImpl.java
@@ -35,6 +35,7 @@ import com.android.os.profiling.anomaly.core.AnomalyReport;
 import com.android.os.profiling.anomaly.core.RuleStorage;
 import com.android.os.profiling.anomaly.core.SignalCollectorRegistry;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -98,6 +99,14 @@ public class AnomalyDetectorControllerImpl
         // Set the new rules without waiting for the save to complete. This makes sure
         // the new rules are applied even if for some reason saving fails.
         setRulesInternal(rules);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<RuleInternal> getRules() {
+        synchronized (mLock) {
+            return Collections.unmodifiableSet(mRules);
+        }
     }
 
     private void setRulesInternal(Set<RuleInternal> rules) {
